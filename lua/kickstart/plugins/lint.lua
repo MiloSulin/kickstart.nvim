54aa83a2440b9f28db7e72ca2088725bc35a3ff1
Milo <milosulin@DESKTOP-FRC0KQ1.>
2024-12-20 03:34:19 +0200

    Tried to get lint working

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,9 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
+        cpp = { 'clangtidy' },
+        c = { 'clangtidy' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -50,9 +52,10 @@ return {
           -- Only run the linter in buffers that you can modify in order to
           -- avoid superfluous noise, notably within the handy LSP pop-ups that
           -- describe the hovered symbol using Markdown.
-          if vim.opt_local.modifiable:get() then
-            lint.try_lint()
-          end
+          lint.try_lint()
+          -- if vim.opt_local.modifiable:get() then
+          --   lint.try_lint()
+          -- end
         end,
       })
     end,
